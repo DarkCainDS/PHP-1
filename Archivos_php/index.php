d2653fd6da6ef3b14c54655a53fedd0ea99bd884
Diego Romero <diego.romero.s@hotmail.com>
2023-09-14 01:39:32 -0300

    first commit

---
+++ Archivos_php/index.php
@@ -0,0 +1,28 @@
+<?php
+
+// echo "Hola, mamá. Estoy programando con PHPCito \n";
+
+$nombre = "Diego";
+$apellido = "Romero";
+
+echo "No, no. Yo me llamo " . $nombre . " " . $apellido . "\n";
+
+// echo "No, no. Yo me llamo $nombre $apellido \n";
+
+echo "El resultado de 4 x 5 es " . (4 * 5) . "\n";
+
+//En Php cuando tu usas comillas dobles “” Php es capaz de leer cualquier variable que tu pongas ahí dentro. Si tu usaras comillas simples ’ ’ no funcionan.
+$personas = [
+    "Carlos" => 22,
+    "Mr. Michi" => 15,
+    "Juan" => 65
+];
+
+//var_dump( $personas);
+# print_r($personas);
+
+/*
+HEllo DarkCAinDS multiline comment
+ */
+
+echo "\n";
